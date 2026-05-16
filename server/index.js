d79904fca5800bf0fae54b593a65e220c53d7a00
Liem Phan <z5419546@student.unsw.edu.au>
2026-05-16 15:20:14 +1000

    chore: creating back-end.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import Anthropic from '@anthropic-ai/sdk';  // no /client.js
+import dotenv from 'dotenv';
+
+dotenv.config();  
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
+});
